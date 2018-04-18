848fa9262ff412f2321ac1cf06c345c5d1b31f88
Oussama BEN BRAHIM <b.brahim.oussama@gmail.com>
2018-04-18 11:14:30 +0000

    use HttpClient

--- src/app/services/posts-list.service.ts
+++ src/app/services/posts-list.service.ts
@@ -1,19 +1,17 @@
 import { Injectable } from '@angular/core';
 import {IPost} from '../domain/ipost';
-import {Http, Response} from '@angular/http';
+import {HttpClient} from '@angular/common/http';
 import 'rxjs/add/operator/map';
 import { Observable } from 'rxjs/Observable';
 @Injectable()
 export class PostsListService {
-  private http: Http;
+  private http: HttpClient;
   private httpGetUrl = 'https://jsonplaceholder.typicode.com/posts/';
-  constructor(private _http: Http) {
+  constructor(private _http: HttpClient) {
     this.http = _http;
   }
   getAllPosts(): Observable<IPost[]> {
-    return this.http.get(this.httpGetUrl).
-    map((resp: Response) => <IPost[]> resp.json()
-    );
+    return this.http.get <IPost[]> (this.httpGetUrl);
   }
 
 }
